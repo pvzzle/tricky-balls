98ec0749a496dff10310bf5e76ef591e8ebe7ef8
pvzzle <mad.pvzzle@gmail.com>
2025-05-06 20:04:51 +0300

    Add draft implementation of algorithm

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var n int = 0
+	fmt.Fscan(reader, &n)
+
+	totalBallsInContainer := make([]int64, n)
+	totalBallsOfSameColor := make([]int64, n)
+
+	for i := range n {
+		for j := range n {
+			var value int64 = 0
+			fmt.Fscan(reader, &value)
+
+			totalBallsInContainer[i] += value
+			totalBallsOfSameColor[j] += value
+		}
+	}
+
+	slices.Sort(totalBallsInContainer)
+	slices.Sort(totalBallsOfSameColor)
+
+	for i := range n {
+		if totalBallsInContainer[i] != totalBallsOfSameColor[i] {
+			fmt.Println("no")
+			return
+		}
+	}
+
+	fmt.Println("yes")
 }
